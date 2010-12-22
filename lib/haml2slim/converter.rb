320dd65e3410c03308255dcdcc27ed45559e1ee1
Fred Wu <ifredwu@gmail.com>
2010-12-23 10:05:24 +1100

    Added more parsing rules.

--- lib/haml2slim/converter.rb
+++ lib/haml2slim/converter.rb
@@ -16,27 +16,34 @@ module Haml2Slim
       indent = line[/^[ \t]*/]
       line.strip!
 
-      converted = case line[0]
-        when ?%, ?., ?# then parse_tag(line)
-        when ?:         then "#{line[1..-1]}:"
-        when ?!         then line.sub('!!!', '! doctype')
-        when ?-, ?=     then line
-        when ?~         then line.sub!(/^~/, "=")
-        when ?/         then line
-        when nil        then ""
-        else "| #{line}"
+      converted = case line[0, 2]
+        when '&=' then line.sub(/^&=/, '==')
+        when '!=' then line.sub(/^!=/, '=')
+        when '-#' then line.sub(/^-#/, '/')
+        else
+          case line[0]
+            when ?%, ?., ?# then parse_tag(line)
+            when ?:         then "#{line[1..-1]}:"
+            when ?!         then line.sub(/^!!!/, '! doctype')
+            when ?-, ?=     then line
+            when ?~         then line.sub(/^~/, '=')
+            when ?/         then line
+            when ?\         then line.sub(/^\\/, '|')
+            when nil        then ""
+            else "| #{line}"
+          end
       end
 
-      if converted.chomp!(" |")
-        converted.sub!(/^\| /, "")
-        converted << " \\"
+      if converted.chomp!(' |')
+        converted.sub!(/^\| /, '')
+        converted << ' \\'
       end
 
       "#{indent}#{converted}\n"
     end
 
     def parse_tag(tag_line)
-      tag_line.sub!(/^%/, "")
+      tag_line.sub!(/^%/, '')
 
       if tag_line_contains_attr = tag_line.match(/(.+)\{(.+)\}/)
         tag, attrs = *tag_line_contains_attr[1..2]
